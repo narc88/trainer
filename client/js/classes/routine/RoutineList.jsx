bc1d8def3b20695beab42da3b34ae3541ec1ccb2
Nicolas Cesarini <ncesarini@grubhub.com>
2016-09-23 17:25:56 -0300

    Routine list

--- client/js/classes/routine/RoutineList.jsx
+++ client/js/classes/routine/RoutineList.jsx
@@ -9,16 +9,33 @@ RoutineList = React.createClass({
 	      	routines: Routines.find({}, { sort: { createdAt: -1 } }).fetch(),
 	    };
 	},
+	_onListItemSelected: function(e) {
+        console.log('Moving to'+e);
+        FlowRouter.go('/routines/'+e);
+    },
 	componentDidMount() {
 	},
 	render() {
-	    return <div>
-	    			<h1>Rutinas</h1>
-	    			<ul class="list-group">
-		                {this.data.routines.map(function(object, i){
-				            return <RoutineListItem routine={object} key={object._id}/>
-				        })}
-			        </ul>
-	            </div>;
+		var _onListItemSelected = this._onListItemSelected;
+	    return 	<div>
+				    <List>
+				      	{this.data.routines.map(function(object, i){
+				      		return <ListItem
+								        leftAvatar={<Avatar onTouchTap={() => _onListItemSelected(object._id)} src='http://res.cloudinary.com/db6uq4jy9/image/upload/v1466101331/c2w7b99g3o21chn5bmxb.jpg' />}
+								        rightIcon={<IconMenu
+												      iconButtonElement={<IconButton><SvgIcons.ActionInfo /></IconButton>}
+												      anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+												      targetOrigin={{horizontal: 'left', vertical: 'bottom'}}
+												    >
+												      	<MenuItem primaryText="Eliminar" />
+												      	<MenuItem primaryText="Editar" />
+												    </IconMenu>}
+								        primaryText={object.name}
+								        secondaryText={object.description}
+								    >
+								    </ListItem>
+			        	})}
+				    </List>
+				</div>;
 	}
 });
